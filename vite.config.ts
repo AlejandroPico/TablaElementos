ae0a904a8ce5544fff611d302b395f3a6ab4b86c
Alejandro Pico <115283749+AlejandroPico@users.noreply.github.com>
2026-07-09 23:36:51 +0200

    rename: actualizar base de GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-const repositoryBase = process.env.GITHUB_ACTIONS ? '/EspectrosAtomicos/' : './';
+const repositoryBase = process.env.GITHUB_ACTIONS ? '/TablaElementos/' : './';
 
 export default defineConfig({
   plugins: [svelte()],
